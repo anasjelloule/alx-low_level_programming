0455fc5580078eafa560564e27389baec1218740
Anas <anasjelloule@gmail.com>
2023-03-17 07:34:59 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-    int a;
-    for (a = 0; a <= 9; a++)
-    {
-        putchar('0' + a);
-    }
-    putchar('\n');
-    return (0);
+int a;
+for (a = 0; a <= 9; a++)
+{
+putchar('0' + a);
+}
+putchar('\n');
+return (0);
 }

0200d122c189802406c951cce77c52001925ef1c
Anas <anasjelloule@gmail.com>
2023-03-17 07:44:03 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+char a;
+for (a = '0'; a <= '9'; a++)
+putchar(a);
+for (a = 'a'; a <= 'f'; a++)
+putchar(a);
+putchar('\n');
+ return (0);
+}

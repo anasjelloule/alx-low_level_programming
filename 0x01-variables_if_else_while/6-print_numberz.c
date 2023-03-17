edc6f5680932282a9a4cf225deed766a18ee622e
Anas <anasjelloule@gmail.com>
2023-03-17 07:33:14 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+    int a;
+    for (a = 0; a <= 9; a++)
+    {
+        putchar('0' + a);
+    }
+    putchar('\n');
+    return (0);
+}

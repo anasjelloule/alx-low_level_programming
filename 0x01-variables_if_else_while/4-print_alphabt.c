e1b4eae4a017d068047adedf2e3a703adcf7f32f
Anas <anasjelloule@gmail.com>
2023-03-17 07:05:46 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/*
+ *Main program
+ */
+int main(void)
+{
+    char a;
+
+    for (a = 'a'; a <= 'z'; a++)
+    {
+        if (a != 'q' && a != 'e')
+            putchar(a);
+    }
+    putchar('\n');
+    return (0);
+}

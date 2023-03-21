c36c241f89b6618930255fe10d668f01e927c525
Anas <anasjelloule@gmail.com>
2023-03-21 09:09:13 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,9 +6,9 @@
  */
 int _islower(int c)
 {
-    char a;
-    for (a = 'a'; a <= 'z'; a++)
-        if (a == c)
-            return 1;
-    return 0;
+char a;
+for (a = 'a'; a <= 'z'; a++)
+if (a == c)
+return 1;
+return 0;
 }

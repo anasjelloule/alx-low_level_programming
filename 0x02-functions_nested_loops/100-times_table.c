bb0ef160fd41350ae4947cd891aca4abe50baeb2
Anas <anasjelloule@gmail.com>
2023-03-21 16:19:46 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,25 +19,25 @@ void print_times_table(int n)
 
 				if (j == 0)
 				{
-					_putchar('0');
+					putchar('0');
 				} else if (prod <= 9)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + prod);
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					putchar(' ');
+					putchar('0' + prod);
 				} else if (prod > 99)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0' + (prod / 100));
-					_putchar('0' + ((prod / 10) % 10));
-					_putchar('0' + (prod % 10));
+					putchar(',');
+					putchar(' ');
+					putchar('0' + (prod / 100));
+					putchar('0' + ((prod / 10) % 10));
+					putchar('0' + (prod % 10));
 				} else
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (prod / 10));
-					_putchar('0' + (prod % 10));
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					putchar('0' + (prod / 10));
+					putchar('0' + (prod % 10));

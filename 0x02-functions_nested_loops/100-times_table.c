330f33c9b3508a26f424d30ceca2ec012762491e
Anas <anasjelloule@gmail.com>
2023-03-21 16:48:31 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,45 +7,44 @@
  */
 void print_times_table(int n)
 {
-    int i;
-    int j;
-    if (n <= 15 && n >= 0)
+int i, j;
+if (n <= 15 && n >= 0)
+{
+for (i = 0; i <= n; i++)
+{
+    for (j = 0; j <= n; j++)
     {
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j <= n; j++)
-            {
-                int prod = j * i;
-                if (j == 0)
-                {
-                    printf("0");
-                }
-                else if (prod <= 9)
-                {
-                    printf(",");
-                    printf(" ");
-                    printf(" ");
-                    printf(" ");
-                    putchar('0' + prod);
-                }
-                else if (prod > 99)
-                {
-                    printf(",");
-                    printf(" ");
-                    putchar('0' + (prod / 100));
-                    putchar('0' + ((prod / 10) % 10));
-                    putchar('0' + (prod % 10));
-                }
-                else
-                {
-                    printf(",");
-                    printf(" ");
-                    printf(" ");
-                    putchar('0' + (prod / 10));
-                    putchar('0' + (prod % 10));
-                }
-            }
-            putchar('\n');
-        }
+int prod = j * i;
+if (j == 0)
+{
+    printf("0");
+}
+else if (prod <= 9)
+{
+    printf(",");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    putchar('0' + prod);
+}
+else if (prod > 99)
+{
+    printf(",");
+    printf(" ");
+    putchar('0' + (prod / 100));
+    putchar('0' + ((prod / 10) % 10));
+    putchar('0' + (prod % 10));
+}
+else
+{
+    printf(",");
+    printf(" ");
+    printf(" ");
+    putchar('0' + (prod / 10));
+    putchar('0' + (prod % 10));
+}
     }
+    putchar('\n');
+}
+}
 }

6c5925e7b0e9860c1b38c0c5832b10c61e8521ef
Anas <anasjelloule@gmail.com>
2023-03-21 09:18:43 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,12 +7,12 @@
  */
 int _isalpha(int c)
 {
-    char a;
-    for (a = 'a'; a <= 'z'; a++)
-        if (a == c)
-            return (1);
-    for (a = 'A'; a <= 'Z'; a++)
-        if (a == c)
-            return (1);
-    return isdigit(c);
+char a;
+for (a = 'a'; a <= 'z'; a++)
+if (a == c)
+return (1);
+for (a = 'A'; a <= 'Z'; a++)
+if (a == c)
+return (1);
+return isdigit(c);
 }

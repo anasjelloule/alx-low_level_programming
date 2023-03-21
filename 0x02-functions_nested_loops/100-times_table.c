71b9e0e835c40a3f2f686c44626ea9070f2a4e5e
Anas <anasjelloule@gmail.com>
2023-03-21 11:31:11 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - times_table
+ * @n: first int
+ * Return: Void
+ */
+void print_times_table(int n)
+{
+    int i, j, m, c = 0;
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            m = j * i;
+            if (m / 10 < 1 && c > 0)
+                putchar(' ');
+            if (m / 100 < 1 && c > 0)
+                putchar(' ');
+            c++;
+            printf("%d", m);
+            if (j < n)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+        c = 0;
+        putchar('\n');
+    }
+}

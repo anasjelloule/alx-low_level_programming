b89569c35e90cf7ca9e32c53921680402c50a053
Anas <anasjelloule@gmail.com>
2023-03-21 16:41:37 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,42 +7,34 @@
  */
 void print_times_table(int n)
 {
-int i;
-int j;
-if (n <= 15 && n >= 0)
-{
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-int prod = j * i;
+    int i;
+    int j;
+    if (n <= 15 && n >= 0)
+    {
+        for (i = 0; i <= n; i++)
+        {
+            for (j = 0; j <= n; j++)
+            {
+                int prod = j * i;
 
-if (j == 0)
-{
-printf("0");
-}
-else if (prod <= 9)
-{
-printf(",");
-printf(" ");
-printf(" ");
-printf(" ");
-putchar('0' + prod);
-}
-else if (prod > 99)
-{
-printf(",");
-printf(" ");
-putchar('0' + (prod / 100));
-putchar('0' + ((prod / 10) % 10));
-putchar('0' + (prod % 10));
-}
-else
-{
-printf(",");
-printf(" ");
-printf(" ");
-putchar('0' + (prod / 10));
-putchar('0' + (prod % 10));
-}
-}
+                if (j == 0)
+                {
+                    printf("0");
+                }
+                else if (prod <= 9)
+                {
+                    printf(",");
+                    printf(" ");
+                    printf(" ");
+                    printf(" ");
+                    putchar('0' + prod);
+                }
+                else if (prod > 99)
+                {
+                    printf(",");
+                    printf(" ");
+                    putchar('0' + (prod / 100));
+                    putchar('0' + ((prod / 10) % 10));
+                    putchar('0' + (prod % 10));
+                }
+                else

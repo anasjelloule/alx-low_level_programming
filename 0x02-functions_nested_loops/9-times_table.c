ba09aed150ad95af652c5ace0bca70b3ddb7ff70
Anas <anasjelloule@gmail.com>
2023-03-21 10:33:12 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - times_table
+ *
+ * Return: Void
+ */
+void times_table(void)
+{
+int i, j, m;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+m = j * i;
+printf("%d", m);
+if (j < 9)
+putchar(',');
+if (m < 10)
+{
+putchar(' ');
+putchar(' ');
+}
+else
+putchar(' ');
+}
+putchar('\n');
+}
+}

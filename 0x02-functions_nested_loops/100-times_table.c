e8fa3dc5ca3bee7cc3f2a24e667d0288f99adcba
Anas <anasjelloule@gmail.com>
2023-03-21 11:37:36 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,25 +7,28 @@
  */
 void print_times_table(int n)
 {
-    int i, j, m, c = 0;
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            m = j * i;
-            if (m / 10 < 1 && c > 0)
-                putchar(' ');
-            if (m / 100 < 1 && c > 0)
-                putchar(' ');
-            c++;
-            printf("%d", m);
-            if (j < n)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        c = 0;
-        putchar('\n');
-    }
+int i, j, m, c = 0;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+m = j * i;
+if (m / 10 < 1 && c > 0)
+putchar(' ');
+if (m / 100 < 1 && c > 0)
+putchar(' ');
+if (m / 1000 < 1 && c > 0)
+putchar(' ');
+
+c++;
+printf("%d", m);
+if (j < n)
+{
+putchar(',');
+putchar(' ');
+}
+}
+c = 0;
+putchar('\n');
+}
 }

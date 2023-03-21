64f7dfbff28addc9d053c932994caf4f5b8cc8cd
Anas <anasjelloule@gmail.com>
2023-03-21 09:24:21 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Check sign
+ * @n: int to check
+ * Return: Void
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf('+');
+return (1);
+}
+if (n < 0)
+{
+printf('-');
+return (-1);
+}
+if (n == 0)
+{
+printf('0');
+return (0);
+}
+}

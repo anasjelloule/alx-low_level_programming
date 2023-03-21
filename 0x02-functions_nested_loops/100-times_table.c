ca5306dd203a9501f2582ce25573e6719c2b7d82
Anas <anasjelloule@gmail.com>
2023-03-21 16:51:39 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,39 +12,39 @@ if (n <= 15 && n >= 0)
 {
 for (i = 0; i <= n; i++)
 {
-    for (j = 0; j <= n; j++)
-    {
+for (j = 0; j <= n; j++)
+{
 int prod = j * i;
 if (j == 0)
 {
-    printf("0");
+printf("0");
 }
 else if (prod <= 9)
 {
-    printf(",");
-    printf(" ");
-    printf(" ");
-    printf(" ");
-    putchar('0' + prod);
+printf(",");
+printf(" ");
+printf(" ");
+printf(" ");
+putchar('0' + prod);
 }
 else if (prod > 99)
 {
-    printf(",");
-    printf(" ");
-    putchar('0' + (prod / 100));
-    putchar('0' + ((prod / 10) % 10));
-    putchar('0' + (prod % 10));
+printf(",");
+printf(" ");
+putchar('0' + (prod / 100));
+putchar('0' + ((prod / 10) % 10));
+putchar('0' + (prod % 10));
 }
 else
 {
-    printf(",");
-    printf(" ");
-    printf(" ");
-    putchar('0' + (prod / 10));
-    putchar('0' + (prod % 10));
+printf(",");
+printf(" ");
+printf(" ");
+putchar('0' + (prod / 10));
+putchar('0' + (prod % 10));
 }
-    }
-    putchar('\n');
+}
+putchar('\n');
 }
 }
 }

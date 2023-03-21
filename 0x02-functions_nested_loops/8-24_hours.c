764040e16430d01e91820b9a7bc5c66028d2171d
Anas <anasjelloule@gmail.com>
2023-03-21 10:01:07 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - jack_bauer
+ *
+ * Return: Void
+ */
+void jack_bauer(void)
+{
+int i, j;
+for (i = 0; i < 2; i++)
+for (j = 0; j < 60; j++)
+{
+if (i < 10)
+printf("0%d:", i);
+if (j < 10)
+printf("0%d", j);
+if (i >= 10)
+printf("%d:", i);
+if (j >= 10)
+printf("%d", j);
+printf("\n");
+}
+}

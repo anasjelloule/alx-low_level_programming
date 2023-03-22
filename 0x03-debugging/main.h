b229087e6ea88ae35cd4b2ba7582838c4f8ab0ec
Anas <anasjelloule@gmail.com>
2023-03-22 04:50:10 -0700

    1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,17 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+/**
+* positive_or_negative - prints if integer is positive or negative
+* Return: 0
+*/
 void positive_or_negative(int i);
-
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c);
 #endif /* MAIN_H */

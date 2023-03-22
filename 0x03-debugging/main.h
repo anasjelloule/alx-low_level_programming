b1e053825ef7e4dc708d29667db6d63571fc33b7
Anas <anasjelloule@gmail.com>
2023-03-22 05:25:10 -0700

    3-print_remaining_days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,18 +1,12 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
-/**
-* positive_or_negative - prints if integer is positive or negative
-* Return: 0
-*/
+
 void positive_or_negative(int i);
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
 int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */

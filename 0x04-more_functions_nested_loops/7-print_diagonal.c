e7aca8e2a097c48b0d54041d2685c74adb5094ba
Anas <anasjelloule@gmail.com>
2023-03-23 05:43:43 -0700

    Add description

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * print_line - print_line
+ * print_diagonal - print_diagonal
  *
  * @n: number of \ to print
  * Return: Always 0.
  */
 void print_diagonal(int n)
 {
-
 int i;
 if (n > 0)
 {

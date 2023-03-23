2e70799bd810ddb2380bfc49ed3190fbfc126dbb
Anas <anasjelloule@gmail.com>
2023-03-23 05:23:38 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - more_numbers
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+int a, i;
+for (i = 0; i < 10; i++)
+{
+for (a = 0; a <= 14; a++)
+{
+if (a > 9)
+_putchar('1');
+_putchar(a % 10 + '0');
+}
+putchar('\n');
+}
+}

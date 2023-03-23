7908cfbfc31ef54306b8cc991fa78494d6894878
Anas <anasjelloule@gmail.com>
2023-03-23 04:55:49 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print_numbers
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+char a;
+for (a = '0'; a <= '9'; a++)
+_putchar(a);
+_putchar('\n');
+}

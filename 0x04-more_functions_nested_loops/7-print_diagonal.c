0c2f1c959f09d4138c22f843c5dcd8f6184816ba
Anas <anasjelloule@gmail.com>
2023-03-23 05:56:16 -0700

    New correction

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,13 @@
 void print_diagonal(int n)
 {
 int i, j;
-if (n > 0)
-{
+if (n < 0)
+return;
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < i; j++)
 _putchar(' ');
 _putchar('\\');
 _putchar('\n');
-}
 }
 }

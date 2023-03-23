383d8d15d225962dd733894d761077823c807231
Anas <anasjelloule@gmail.com>
2023-03-23 06:49:18 -0700

    9-fizz_buzz.c Last One

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,17 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 continue;
 }
 else if (i % 3 == 0)
-printf("Fizz ");
+printf("Fizz");
 else if (i % 5 == 0)
-printf("Buzz ");
-else if (i != 100)
-printf("%d ", i);
+printf("Buzz");
+else
+printf("%d", i);
+if (i != 100)
+printf(" ");
 }
 return (0);
 }

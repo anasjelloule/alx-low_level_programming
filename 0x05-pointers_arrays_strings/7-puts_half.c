5b217a3d039e009e1151bf8d53f3ecde44e08cd6
Anas <anasjelloule@gmail.com>
2023-03-27 10:45:21 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,14 @@
+#include "2-strlen.c"
+#include "main.h"
+/**
+ * puts_half -  Print
+ *
+ * @str: String
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+int i, j = _strlen(str);
+for (i = (j / 2); i < j; i++)
+putchar(*(str + i));
+}

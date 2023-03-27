0d410808f6b852aaed90a5ebb53a3d914ae2addd
Anas <anasjelloule@gmail.com>
2023-03-27 07:58:00 -0700

    3-puts.c third solution

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts -  swap int
+ *
+ * @str: String
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+void _puts(char *str);
 /**
  * _putchar - writes the character c to std
  * @c: parameter to compare
@@ -15,6 +16,5 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-
 
 #endif

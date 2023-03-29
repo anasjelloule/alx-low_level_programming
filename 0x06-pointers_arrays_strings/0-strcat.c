d73b908cbd0c2741ad04c5ab31edde4bf672e995
Anas <anasjelloule@gmail.com>
2023-03-29 04:50:13 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat -  Concat String
+ *
+ * @des: string
+ * @src: string
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j = 0;
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+++i;
+j++;
+}
+return dest;
+}

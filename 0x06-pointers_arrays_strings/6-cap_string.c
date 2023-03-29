ba1c677d25892d551e2efe1041fd9966c9ba5cd2
Anas <anasjelloule@gmail.com>
2023-03-29 09:18:23 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * cap_string -  cap String
+ *
+ * @str: char
+ *
+ * Return: Always 0.
+ */
+char *cap_string(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+str[i + 1] = str[i + 1] - 32;
+}

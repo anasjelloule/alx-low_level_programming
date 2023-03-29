bbe12cfc52c04070fd8d0633bcaf84885d8ce554
Anas <anasjelloule@gmail.com>
2023-03-29 08:14:55 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,6 @@ int _strcmp(char *s1, char *s2)
 {
 int i, j, f = 0;
 for (i = 0; s2[i] != '\0'; i++)
-{
-}
-for (j = 0; s1[j] != '\0'; j++)
-{
-}
-if (i > j)
-return (-1);
-if (i < j)
-return (1);
-for (i = 0; s2[i] != '\0'; i++)
-if (s1[i] != s2[i])
-f++;
-return (f);
+if (s1[i] - s2[i] != 0)
+return (s1[i] - s2[i]);
 }

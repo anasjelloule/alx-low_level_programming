9f16f19b06ec2d1ccab42aa920921a120858f7c7
Anas <anasjelloule@gmail.com>
2023-03-29 11:30:50 -0700

    100-rot13.c Second solution

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * rot13 -  Encrypte string
+ *
+ * @str: char
+ *
+ * Return: Always 0.
+ */
+char *rot13(char *str)
+{
+    char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+    char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+    int i, j = 0;
+ 
+    return (str);
+}

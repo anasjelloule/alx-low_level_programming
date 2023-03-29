3fc37537203d90832ce2c56b042856508b25e034
Anas <anasjelloule@gmail.com>
2023-03-29 09:21:21 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,4 +13,5 @@ for (i = 0; str[i] != '\0'; i++)
 if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 str[i + 1] = str[i + 1] - 32;
+return (str);
 }

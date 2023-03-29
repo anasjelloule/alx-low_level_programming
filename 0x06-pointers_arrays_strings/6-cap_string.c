3583891735066073d55c15c258bae0b755c22c9e
Anas <anasjelloule@gmail.com>
2023-03-29 09:34:40 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,20 @@
 #include "main.h"
 /**
+ * _checkOf - check if special  character
+ * @c: character to Get
+ * Return: 0 ou 1
+ */
+int _checkOf(char c)
+{
+int i;
+char checkarr[13] = {'\n', '\t', ' ', '.', ',', ';', '!', '?', '(',
+ ')', '{', '}'};
+for (i = 0; i < 13; i++)
+if (checkarr[i] == c)
+return (1);
+return (0);
+}
+/**
  * cap_string -  cap String
  *
  * @str: char
@@ -10,8 +25,7 @@ char *cap_string(char *str)
 {
 int i;
 for (i = 0; str[i] != '\0'; i++)
-if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-str[i + 1] = str[i + 1] - 32;
+if (_checkOf(str[i - 1]) == 1 && str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 32;
 return (str);
 }

62ac98bec8e744f91cc83c6f3f9658dbbaf6ba5e
Anas <anasjelloule@gmail.com>
2023-03-29 11:31:14 -0700

    100-rot13.c Second solution

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,9 +8,15 @@
  */
 char *rot13(char *str)
 {
-    char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
-    char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
-    int i, j = 0;
- 
-    return (str);
+char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+int i, j = 0;
+for (i = 0; str[i] != '\0'; i++)
+for (j = 0; part1[j] != '\0'; j++)
+if (str[i] == part1[j])
+{
+str[i] = part2[j];
+break;
+}
+return (str);
 }

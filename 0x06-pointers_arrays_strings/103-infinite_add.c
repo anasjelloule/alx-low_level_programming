d0a50bc79ff933f02f9eb58830cae02c4690a5eb
Anas <anasjelloule@gmail.com>
2023-03-30 04:39:17 -0700

    103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * infinite_add -  Encrypte string
+ *
+ * @n1: char
+ * @n2: char
+ * @r: char
+ * @size_r: char
+ *
+ * Return: char.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+return (n1);
+}

f12cf6f1073f567d114d0e690aef94c096f89983
Anas <anasjelloule@gmail.com>
2023-03-30 04:43:25 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,5 +11,10 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+int i;
+for (i = 0; n1[i] != '\0'; i++)
+for (i = 0; n2[i] != '\0'; i++)
+for (i = 0; r[i] != '\0' && i < size_r; i++)
+return (n1);
 return (n1);
 }

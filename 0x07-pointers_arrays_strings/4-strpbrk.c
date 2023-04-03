64919a161922a954651374a395ba6311db260771
Anas <anasjelloule@gmail.com>
2023-04-03 08:37:04 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strpbrk - search for the character c
+ * @s: string to cop from src
+ * @accept: char to copy to dest
+ * Return: 1 or 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+for (j = 0; accept[j] != '\0'; j++)
+if (s[i] == accept[j])
+return (s + i);
+return (0);
+}

9e69b3534dc8b9ba170edebc4f6d9fe6a2667b48
Anas <anasjelloule@gmail.com>
2023-04-05 03:00:22 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  _puts_recursion - Print the String
+ * @s: string to cop from src
+ * Return: return 0
+ */
+void _puts_recursion(char *s)
+{
+if (*s != '\0')
+    _putchar(*s++);
+else
+    return;
+_puts_recursion(s);
+}

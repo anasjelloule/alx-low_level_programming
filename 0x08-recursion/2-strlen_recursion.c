5f85681035204b2816e4b63396313c4939e1d33d
Anas <anasjelloule@gmail.com>
2023-04-05 04:04:02 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  _strlen_recursion - Print the String
+ * @s: string to cop from src
+ * Return: return Length of string
+ */
+int _strlen_recursion(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+return (i);
+}

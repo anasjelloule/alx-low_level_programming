92c3f5aac7b25509cade2dfd5c5de3b62c9766a3
Anas <anasjelloule@gmail.com>
2023-04-05 05:11:30 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  _pow_recursion - Calcul Power-of x by y
+ * @x: Number to calculate
+ * @y: Number to calculate
+ * Return: Power
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+if (y > 1)
+return (_pow_recursion(x, --y) * x);
+}

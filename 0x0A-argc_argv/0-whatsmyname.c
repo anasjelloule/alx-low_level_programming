52887f1f5b56ea9238c4733862392b47ed6df896
Anas <anasjelloule@gmail.com>
2023-04-06 08:10:16 -0700

    0x09-static_libraries

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * @argc: Number of arguments passed
+ * @argv: Arguments passed
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * @argc: Number of arguments passed
+ * @argv: Arguments passed
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}

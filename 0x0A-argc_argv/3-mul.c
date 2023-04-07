6f506d52a6d2555ec1cb9027a2a1939aa0a3be18
Anas <anasjelloule@gmail.com>
2023-04-07 05:04:58 -0700

    3-mul.c Fixed bug

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,9 +9,9 @@
  */
 int main(int argc, char *argv[])
 {
-if (argc < 2)
-printf("Error\n");
-else
+if (argc == 3)
 printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+else
+printf("Error\n");
 return (0);
 }

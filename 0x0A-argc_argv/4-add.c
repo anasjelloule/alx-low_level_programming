43f6bad9a642e3bbdbb7d6ec95fcd0a56445d539
Anas <anasjelloule@gmail.com>
2023-04-07 06:44:11 -0700

    4-add.c Fixed bug

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "main.h"
+/**
+ *  checkstring - check a string if it's int
+ * @s: string to check
+ * Return: return 0
+ */
+int checkstring(char *s)
+{
+int i = 0;
+for (; s[i] != '\0'; i++)
+{
+if (!isdigit(s[i]))
+{
+return (0);
+}
+}
+return (1);
+}
 /**
  * main - check the code
  * @argc: Number of arguments passed
@@ -17,7 +35,7 @@ return (0);
 }
 for (i = 1; i < argc; i++)
 {
-if (atoi(argv[i]) == 0)
+if (!checkstring(argv[i]))
 {
 printf("Error\n");
 return (1);

b4188eecd9ef73954c63bb5baf2bdb0898a85734
Anas <anasjelloule@gmail.com>
2023-04-13 08:43:15 -0700

    0-malloc_checked.c Last Solution

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,12 +4,12 @@
 /**
  * malloc_checked - Spaces checked
  * @b: parameter
- * Return: 1 or 0
+ * Return: a pointer or void
  */
 void *malloc_checked(unsigned int b)
 {
 void *check = malloc(b);
 if (check == NULL)
 exit(98);
-return (malloc(b));
+return (check);
 }

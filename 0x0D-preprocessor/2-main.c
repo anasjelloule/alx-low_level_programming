8387e500b7bb891f618d1db60062dd17a3d94c48
Anas <anasjelloule@gmail.com>
2023-04-17 11:54:30 -0700

    2-main.c fix Betty

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @ac: Number of arguments passed
+ * @av: Arguments passed
+ * Return: Always 0.
+ */
+int main(int ac, char *av[])
+{
+printf("%s\n", __FILE__);
+return (0);
+}

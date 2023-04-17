1bfc70fc91594428945dea14fd65d99a6ca2187b
Anas <anasjelloule@gmail.com>
2023-04-17 13:11:45 -0700

    dog.h

---
+++ dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct with dog info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: store different data types
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif

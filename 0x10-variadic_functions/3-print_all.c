3eee3bcf70f8483e7e6b4ad9b2ba85cbb1a27faf
Anas <anasjelloule@gmail.com>
2023-04-20 09:14:54 -0700

    3-print_all.c Find new solution

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,91 +1,50 @@
-#include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * print_c - prints char
- * @a: list to give
- * Return: always 0
- */
-int print_c(va_list a)
-{
-	printf("%c", va_arg(a, int));
-	return (0);
-}
-/**
- * print_i - prints int
- * @a: list to give
- * Return: always 0
- */
-int print_i(va_list a)
-{
-	printf("%d", va_arg(a, int));
-	return (0);
-}
-/**
- * print_f - prints float
- * @a: list to give
- * Return: always 0
- */
-int print_f(va_list a)
-{
-	printf("%f", va_arg(a, double));
-	return (0);
-}
-/**
- * print_s - prints string
- * @a: list to give
- * Return: always 0
- */
-int print_s(va_list a)
-{
-	char *s;
-
-	s = va_arg(a, char *);
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return (0);
-	}
-	printf("%s", s);
-	return (0);
-}
-/**
  * print_all - prints all
- * @format: format string that says arg types
- *
+ * @format: formats of arg
  */
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
-	int i, j;
-	char *sep = "";
-	char *sep2 = ", ";
-	va_list anyArgs;
-	printer ops[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"s", print_s},
-		{"f", print_f},
-		{NULL, NULL}
-	};
 
-	va_start(anyArgs, format);
-	i = 0;
-	while (format != NULL && format[i])
-	{
-		j = 0;
-		while (ops[j].f != NULL)
-		{
-			if (format[i] == *(ops[j].c))
-			{
-				printf("%s", sep);
-				ops[j].f(anyArgs);
-			}
-			j++;
-		}
-		sep = sep2;
-		i++;
-	}
-	printf("\n");
-	va_end(anyArgs);
+int i = 0;
+char *str, *sep = "";
+
+va_list list;
+
+va_start(list, format);
+if (format)
+{
+while (format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", sep, va_arg(list, int));
+break;
+case 'i':
+printf("%s%d", sep, va_arg(list, int));
+break;
+case 'f':
+printf("%s%f", sep, va_arg(list, double));
+break;
+case 's':
+str = va_arg(list, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", sep, str);
+break;
+default:
+i++;
+continue;
+}
+sep = ", ";
+i++;
+}
+}
+
+printf("\n");
+va_end(list);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,18 +11,4 @@ void print_all(const char * const format, ...);
  * Return: 1 or 0
  */
 int _putchar(char c);
-int print_c(va_list a);
-int print_i(va_list a);
-int print_f(va_list a);
-int print_s(va_list a);
-/**
- * struct op - struct for 3
- * @c: character to check
- * @f: function to point to
- */
-typedef struct op
-{
-	char *c;
-	int (*f)(va_list a);
-} printer;
 #endif

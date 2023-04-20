edd2a9e68aa6819c7edf1e0f92638b1987b2df75
Anas <anasjelloule@gmail.com>
2023-04-20 06:36:58 -0700

    2-print_strings.c forgot step check

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,14 +12,18 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
-
+char *str;
 va_list lst;
 va_start(lst, n);
 for (i = 0; i < n; i++)
 {
+str = va_arg(lst, char *);
 if (i > 0 && separator != NULL)
 printf("%s", separator);
-printf("%s", va_arg(lst, char *));
+if (str != NULL)
+printf("%s", str);
+else
+printf("(nil)");
 }
 printf("\n");
 va_end(lst);

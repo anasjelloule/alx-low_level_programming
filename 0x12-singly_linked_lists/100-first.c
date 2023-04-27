51d9bd95e046c18e18cec7151f821fb6cce96c9b
Anas <anasjelloule@gmail.com>
2023-04-27 08:48:47 -0700

    100-first.c 2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * before_main -   prints Before main
+ * Return: the number of nodes
+ */
 void __attribute__((constructor)) before_main()
 {
 printf("You're beat! and yet, you must allow,\n");

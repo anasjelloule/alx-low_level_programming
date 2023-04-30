f4ed9650e18c96e155c049afc47f486f75909898
Anas <anasjelloule@gmail.com>
2023-04-30 11:33:40 -0700

    100-reverse_listint.c Easy one

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_listint_safe -  Print all linked list
+ * @head: pointer to listint_t structure
+ * Return: the number of nodes
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *anas;
+listint_t *h;
+
+anas = 0;
+while (*head != NULL)
+{
+h = (*head)->next;
+(*head)->next = anas;
+anas = (*head);
+(*head) = h;
+}
+(*head) = anas;
+return (*head);
+}

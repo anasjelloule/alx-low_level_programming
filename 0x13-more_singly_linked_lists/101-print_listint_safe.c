d4ea758c39e54d3d563298559fda64168ccc603c
Anas <anasjelloule@gmail.com>
2023-04-30 12:12:47 -0700

    Back to print_listint_safe 2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,8 +9,6 @@ size_t print_listint_safe(const listint_t *head)
 size_t c;
 listint_t *anas = (listint_t *)head;
 c = 0;
-if (head == NULL)
-exit(98);
 while (anas && anas > anas->next)
 {
 printf("[%p] %d\n", (void *)anas, anas->n);
@@ -22,6 +20,7 @@ if (anas)
 printf("[%p] %d\n", (void *)anas, anas->n);
 printf("-> [%p] %d\n", (void *)anas->next, anas->next->n);
 c++;
+exit(98);
 }
 return (c);
 }

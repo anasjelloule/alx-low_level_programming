76ab3933f8f7a6de6172669af1baae13e68cbbf0
Anas <anasjelloule@gmail.com>
2023-04-30 08:48:10 -0700

    101-print_listint_safe.c 2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,6 +8,7 @@ size_t print_listint_safe(const listint_t *head)
 {
 if (head)
 {
+printf("[%p] %d\n", (void *)head, head->n);
 if (head->next < head)
 return (1 + print_listint_safe(head->next));
 else

2d1364ed3b891507c4aff7a288ce8d854b15b2de
Anas <anasjelloule@gmail.com>
2023-04-30 09:55:34 -0700

    101-print_listint_safe.c new algorithm

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,16 +6,20 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-if (head)
+size_t c;
+listint_t *anas = (listint_t *)head;
+c = 0;
+while (anas && anas > anas->next)
 {
-printf("[%p] %d\n", (void *)head, head->n);
-if (head->next < head)
-return (1 + print_listint_safe(head->next));
-else
+printf("[%p] %d\n", (void *)anas, anas->n);
+anas = anas->next;
+c++;
+}
+if (anas)
 {
-printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-return (1);
+printf("[%p] %d\n", (void *)anas, anas->n);
+printf("-> [%p] %d\n", (void *)anas->next, anas->next->n);
+c++;
 }
-}
-return (0);
+return (c);
 }

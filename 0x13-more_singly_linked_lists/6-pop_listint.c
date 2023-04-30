62be927c52dbfd1544a1fa3b00c9de2f1bf57bb6
Anas <anasjelloule@gmail.com>
2023-04-30 06:45:52 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * pop_listint -  remove the head of listint_t linked list
+ * @head: pointer to listint_t structure
+ * Return: the number of nodes
+ */
+int pop_listint(listint_t **head)
+{
+listint_t *anas;
+unsigned int c = 0;
+anas = malloc(sizeof(listint_t));
+if (anas == NULL || head == NULL || *head == NULL)
+return (0);
+anas = *head;
+*head = anas->next;
+return (anas->n);
+}

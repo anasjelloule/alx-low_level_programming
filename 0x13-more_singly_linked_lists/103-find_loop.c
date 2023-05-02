7c79637b27ba51835227138d55fe0457f18b9ac6
Anas <anasjelloule@gmail.com>
2023-05-02 12:50:26 -0700

    Loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,9 @@
+/**
+* find_listint_loop - Find List Loop
+* @head: pointer to listint_t structure
+* Return: the number of nodes
+*/
+listint_t *find_listint_loop(listint_t *head)
+{
+return (*head);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,7 @@ struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t print_listint_safe(const listint_t *head);
+listint_t *find_listint_loop(listint_t *head);
 int pop_listint(listint_t **head);
 size_t listint_len(const listint_t *h);
 size_t free_listint_safe(listint_t **h);

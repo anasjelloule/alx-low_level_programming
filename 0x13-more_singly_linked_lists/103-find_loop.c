8cc4f7d105a22b0d457a80ad8b3a266beddbba66
Anas <anasjelloule@gmail.com>
2023-05-02 13:28:21 -0700

    Loop 4

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -7,5 +7,23 @@
 listint_t *find_listint_loop(listint_t *head)
 {
 listint_t *anas = head;
-return (anas);
+listint_t *anas2 = head;
+if (!head)
+return (NULL);
+while (anas && anas2 && anas2->next)
+{
+anas2 = anas2->next->next;
+anas = anas->next;
+if (anas2 == anas)
+{
+anas = head;
+while (anas != anas2)
+{
+anas = anas->next;
+anas2 = anas2->next;
+}
+return (anas2);
+}
+}
+return (NULL);
 }

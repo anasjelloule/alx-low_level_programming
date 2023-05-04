56a37ec9de11b6424b89d578488f5aa1b093182e
Anas <anasjelloule@gmail.com>
2023-05-04 12:54:58 -0700

    2-get_bit.c forgot some cases

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,6 +9,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 if (index >= (sizeof(unsigned long int) * 8))
 return (-1);
-
-return (n);
+if ((n & (1 << index)) == 0)
+return (0);
+return (1);
 }

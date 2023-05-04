d0320ff05ae48186d912edf9cb61295c10050b50
Anas <anasjelloule@gmail.com>
2023-05-04 13:07:21 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *  unsigned -  sets the value of a bit to 0 at a given index
+ * @n: Number
+ * @ù:  to set the value at indices start at 0.
+ * Return: integer
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+  unsigned long int anas = n ^ m;
+return (anas);
+}

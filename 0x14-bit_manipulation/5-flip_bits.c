8729c655471d075257b2d43e0c00878d7ddfedd7
Anas <anasjelloule@gmail.com>
2023-05-04 13:08:28 -0700

    5-flip_bits.c 2

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,6 +7,12 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-  unsigned long int anas = n ^ m;
-return (anas);
+unsigned long int anas = n ^ m, anas2 = 0;
+
+while (anas > 0)
+{
+anas2 += (anas & 1);
+anas >>= 1;
+}
+return (anas2);
 }

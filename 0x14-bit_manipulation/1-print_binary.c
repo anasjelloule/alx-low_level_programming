dc7f8cc3af2f478a938b41d09d91bdbe53ec4243
Anas <anasjelloule@gmail.com>
2023-05-04 07:00:16 -0700

    1-print_binary.c by recursion 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,7 +3,7 @@
 #include "main.h"
 /**
  *  print_binary - function that prints the binary representation of a number
- * @b: Number arguments
+ * @n: Number to convert
  * Return: unsigned integer
  */
 void print_binary(unsigned long int n)

400ac1d12919bec8985c25037cfbb0d640ff41d5
Anas <anasjelloule@gmail.com>
2023-05-08 09:55:15 -0700

    Last One

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the code
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+return (0);
+}
